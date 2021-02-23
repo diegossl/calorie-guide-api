b94d8372271d8b58273d873168fecc4473fea25d
Diego <diegossl94@gmail.com>
2021-02-22 22:06:06 -0300

    feat: module attributes added

--- src/features/food/food.module.ts
+++ src/features/food/food.module.ts
@@ -1,4 +1,10 @@
+import { FoodController } from './food.controller'
+import { FoodService } from './food.service'
 import { Module } from '@nestjs/common'
 
-@Module({})
+@Module({
+  providers: [FoodService],
+  controllers: [FoodController],
+  exports: [FoodService]
+})
 export class FoodModule {}
